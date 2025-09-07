0fb35567bc1e1176a5973c07b3a120b9be662f3d
ZureMatt <matthieukamphuis44@gmail.com>
2025-09-07 21:54:20 +0200

    Actually one line

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace One_Line
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿using System; namespace One_Line {class Program {static void Main(string[] args) {Console.WriteLine("Hello World!");}}}
